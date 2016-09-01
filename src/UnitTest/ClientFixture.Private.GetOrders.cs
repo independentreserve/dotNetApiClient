71b7196d5f3ad23a02e0f8402f2ef325b078b447
Aleksey B <aleksey-baryshnikov@users.noreply.github.com>
2016-09-01 13:19:20 +0600

    #2356 API: make currencies optional

--- src/UnitTest/ClientFixture.Private.GetOrders.cs
+++ src/UnitTest/ClientFixture.Private.GetOrders.cs
@@ -24,6 +24,23 @@ namespace UnitTest
         }
 
         [Test]
+        public void GetOpenOrdersNoCurrencies()
+        {
+            using (var client = CreatePrivateClient())
+            {
+                Page<BankHistoryOrder> page = client.GetOpenOrders(null, null, 1, 10);
+
+                Assert.IsNotNull(page);
+
+                Assert.AreEqual(page.PageSize, 10);
+                Assert.IsTrue(page.TotalItems > 0);
+
+                Assert.IsTrue(page.Data.Any());
+                Assert.IsTrue(page.Data.Count() <= 10);
+            }
+        }
+
+        [Test]
         public void GetClosedOrders()
         {
             using (var client = CreatePrivateClient())
@@ -41,11 +58,45 @@ namespace UnitTest
         }
 
         [Test]
+        public void GetClosedOrdersNoCurrencies()
+        {
+            using (var client = CreatePrivateClient())
+            {
+                Page<BankHistoryOrder> page = client.GetClosedOrders(null, null, 1, 10);
+
+                Assert.IsNotNull(page);
+
+                Assert.AreEqual(page.PageSize, 10);
+                Assert.IsTrue(page.TotalItems > 0);
+
+                Assert.IsTrue(page.Data.Any());
+                Assert.IsTrue(page.Data.Count() <= 10);
+            }
+        }
+
+        [Test]
         public void GetClosedFilledOrders()
         {
             using (var client = CreatePrivateClient())
             {
                 Page<BankHistoryOrder> page = client.GetClosedFilledOrders(CurrencyCode.Xbt, CurrencyCode.Usd, 1, 10);
+
+                Assert.IsNotNull(page);
+
+                Assert.AreEqual(page.PageSize, 10);
+                Assert.IsTrue(page.TotalItems > 0);
+
+                Assert.IsTrue(page.Data.Any());
+                Assert.IsTrue(page.Data.Count() <= 10);
+            }
+        }
+
+        [Test]
+        public void GetClosedFilledOrdersNoCurrencies()
+        {
+            using (var client = CreatePrivateClient())
+            {
+                Page<BankHistoryOrder> page = client.GetClosedFilledOrders(null, null, 1, 10);
 
                 Assert.IsNotNull(page);
 
