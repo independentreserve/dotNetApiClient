bd62d961db7aeb3fcb1074b1502d4a01bcfff836
Brent <brent-ir@users.noreply.github.com>
2018-03-15 10:22:10 +1100

    restore assemblyinfo

--- src/DotNetClientApi/Properties/AssemblyInfo.cs
+++ src/DotNetClientApi/Properties/AssemblyInfo.cs
@@ -1,3 +1,22 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-[assembly:InternalsVisibleTo("UnitTest")]
+[assembly: AssemblyTitle("DotNetClientApi")]
+[assembly: AssemblyDescription("Independent Reserve .NET Client")]
+[assembly: AssemblyCompany("Independent Reserve")]
+[assembly: AssemblyProduct("IrClientApi")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("dev build")]
+
+[assembly: InternalsVisibleTo("UnitTest")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyConfiguration("Release")]
+#endif
+
