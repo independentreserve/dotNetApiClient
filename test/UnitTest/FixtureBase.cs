c12eefb8e804c37ba23f24764401b11374430b57
Brent W <brent-ir@users.noreply.github.com>
2018-03-18 11:29:59 +1100

    split tests into categories

--- test/UnitTest/FixtureBase.cs
+++ test/UnitTest/FixtureBase.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using System.Threading;
+using IndependentReserve.DotNetClientApi;
 using NUnit.Framework;
 
 namespace UnitTest
@@ -10,6 +11,10 @@ namespace UnitTest
     [TestFixture]
     public abstract class FixtureBase
     {
+        protected const string ApiKey = "3de4047d-81e4-421e-8402-9dddb8971b32";
+        protected const string ApiSecret = "6195d2ef6c0e4c6d94bb0c8a982ef825";
+        protected const string BaseUrl = "http://api.ir.localhost";
+
         /// <summary>
         ///     The test fixture setup method.
         /// </summary>
@@ -19,6 +24,27 @@ namespace UnitTest
             //  set culture to en-US
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
             Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+        }
+    }
+
+    [Category("Private")]
+    public class PrivateFixtureBase : FixtureBase
+    {
+
+        protected Client CreatePrivateClient()
+        {
+            return Client.CreatePrivate(ApiKey, ApiSecret, BaseUrl);
+        }
+
+    }
+
+    [Category("Public")]
+    public class PublicFixtureBase : FixtureBase
+    {
+
+        protected Client CreatePublicClient()
+        {
+            return Client.CreatePublic(BaseUrl);
         }
     }
 }
