fd32733b01b08dc73aa8d3ada70c16683cca3ff6
Brent <brent-ir@users.noreply.github.com>
2018-06-05 13:27:49 +1000

    #26512 let sample app pull config from env var

---
+++ test/SampleApplication/Config/AppSettingsConfigProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using IndependentReserve.DotNetClientApi;
+
+namespace SampleApplication
+{
+    public class AppSettingsConfigProvider : IConfigProvider
+    {
+        public ApiConfig Get()
+        {
+            var creds = new ApiCredential
+            {
+                Key = ConfigurationManager.AppSettings["apiKey"]
+                ,Secret = ConfigurationManager.AppSettings["apiSecret"]
+            };
+
+            var config = new ApiConfig
+            {
+                BaseUrl = ConfigurationManager.AppSettings["apiUrl"]
+                ,Credential = creds
+            };
+
+            return config;
+        }
+    }
+}
