e1f3b21cef2ff1101f53a373f8a51c39eff3eca5
Aman B <2719492+Amanbek@users.noreply.github.com>
2019-05-06 13:49:43 +0600

    IR-670 missing file

---
+++ src/DotNetClientApi/Data/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IndependentReserve.DotNetClientApi.Data
+{
+    public class Event
+    {
+        public DateTimeOffset Published { get; set; }
+
+        public string Description { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTimeOffset Start { get; set; }
+
+        public int EstimationDurationMinutes { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
