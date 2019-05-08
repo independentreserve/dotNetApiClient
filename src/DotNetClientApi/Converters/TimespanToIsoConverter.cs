1784cacfb732a2ecd2e90642ee9bb2882ea23bd2
Aman B <2719492+Amanbek@users.noreply.github.com>
2019-05-08 09:17:55 +0600

    IR-670 TimSpan

---
+++ src/DotNetClientApi/Converters/TimespanToIsoConverter.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml;
+
+namespace IndependentReserve.DotNetClientApi.Converters
+{
+    // https://stackoverflow.com/questions/12466188/how-do-i-convert-an-iso8601-timespan-to-a-c-sharp-timespan
+    public class TimeSpanToIsoConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            if (!(value is TimeSpan))
+            {
+                throw new JsonSerializationException("Expected TimeSpan object value.");
+            }
+
+            var timeSpan = (TimeSpan)value;
+            writer.WriteValue(XmlConvert.ToString(timeSpan));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return XmlConvert.ToTimeSpan((string)reader.Value);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(TimeSpan);
+        }
+    }
+}
