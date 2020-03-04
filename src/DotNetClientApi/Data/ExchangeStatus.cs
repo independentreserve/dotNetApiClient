c7d7b39d1f14173318e44dc928812dda05f95e58
Brent <brent-ir@users.noreply.github.com>
2020-03-04 14:03:46 +1100

    IR-1533 docs

--- src/DotNetClientApi/Data/ExchangeStatus.cs
+++ src/DotNetClientApi/Data/ExchangeStatus.cs
@@ -5,17 +5,23 @@ namespace IndependentReserve.DotNetClientApi.Data
 
     public class ExchangeStatus
     {
-        public bool IsOverallHealthy { get; set; }
         /// <summary>
-        /// The collection contains entries indicating whether the trading is enabled for corresponding currency
+        /// Overall health of the exchange
+        /// </summary>
+        public bool IsOverallHealthy { get; set; }
+
+        /// <summary>
+        /// Indicates whether trading is enabled for each currency
         /// </summary>
         public Dictionary<CurrencyCode, bool> TradingEnabled { get; set; }
+
         /// <summary>
-        /// The collection contains entries indicating whether withdrawals can be made in a corresponding currency
+        ///Indicates whether withdrawals are enabled for each currency
         /// </summary>
         public Dictionary<CurrencyCode, bool> WithdrawalsEnabled { get; set; }
+
         /// <summary>
-        /// The collection contains entries indicating whether deposits can be made in a corresponding currency
+        /// Indicates whether deposits are enabled for each currency
         /// </summary>
         public Dictionary<CurrencyCode, bool> DepositsEnabled { get; set; }
     }
