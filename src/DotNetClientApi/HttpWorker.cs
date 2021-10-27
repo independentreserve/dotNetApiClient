f5d650617e14c200ddee5eb2b674801b931e9f6f
Eduard Sergeev <eduard.sergeev@gmail.com>
2021-10-27 11:41:58 +1100

    Add an option to add additional request headers
    To be passed to API with every request

--- src/DotNetClientApi/HttpWorker.cs
+++ src/DotNetClientApi/HttpWorker.cs
@@ -6,7 +6,6 @@ using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,8 +32,9 @@ namespace IndependentReserve.DotNetClientApi
             _version = typeof(HttpWorker).Assembly.GetName().Version.ToString();
         }
 
-        public HttpWorker(Uri baseUri)
+        public HttpWorker(Uri baseUri, IDictionary<string, string> headers = null)
         {
+            (string, string)[] f = {};
             HttpClientHandler handler = new HttpClientHandler
             {
                 AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
@@ -45,6 +45,10 @@ namespace IndependentReserve.DotNetClientApi
             _client.DefaultRequestHeaders.UserAgent.ParseAdd("irDotNetClient " + _version);
             _client.DefaultRequestHeaders.AcceptEncoding.ParseAdd("gzip");
             _client.DefaultRequestHeaders.AcceptEncoding.ParseAdd("deflate");
+            foreach(var header in headers ?? Enumerable.Empty<KeyValuePair<string, string>>())
+            {
+                _client.DefaultRequestHeaders.Add(header.Key, header.Value);
+            }
         }
 
         /// <summary>
